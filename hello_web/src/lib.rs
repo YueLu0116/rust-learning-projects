8fe4ab7b43d56335472400493ceca7b20b1d87fb
YueLu0116 <bit_drinkwater@163.com>
2021-11-11 23:59:58 +0800

    Finish the second part of the final project

--- hello_web/src/lib.rs
+++ hello_web/src/lib.rs
@@ -3,22 +3,25 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::mpsc;
 
+
 pub struct ThreadPool{
     workers:Vec<Worker>,
     sender:mpsc::Sender<Job>,
 }
 
 impl ThreadPool{
+    
     pub fn new(size:usize)->ThreadPool{
         assert!(size > 0);
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size); // preallocates space in the vector.
         for id in 0..size{
-            workers.push(Worker::new(id, Arc.clone(&receiver))); // bump the reference count
+            workers.push(Worker::new(id, Arc::clone(&receiver))); // bump the reference count
         }
-        ThreadPool{workers}
+        ThreadPool{workers, sender}
     }
+
     pub fn execute<F>(&self, f:F)
     where 
         F:FnOnce() + Send + 'static,
@@ -32,7 +35,7 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 
 struct Worker{
     id : usize,
-    thread : JoinHandle<()>,
+    thread : thread::JoinHandle<()>,
 }
 
 impl Worker{
@@ -44,29 +47,4 @@ impl Worker{
         });
         Worker{id, thread}
     }
-}
-
-pub fn handle_connection(mut stream : TcpStream){
-    let mut buffer = [0;1024];
-    stream.read(&mut buffer).unwrap();
-    
-    let get = b"GET / HTTP/1.1\r\n";  // b->bytes
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "html/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
 }
--- hello_web/src/main.rs
+++ hello_web/src/main.rs
@@ -1,15 +1,17 @@
 use hello_web::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpListener;
-use std::net::TcpStream;
 use std::fs;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        handle_connection(stream);
-        println!("Tcp connection established");
+        pool.execute(||{
+            handle_connection(stream);
+        });
     }
 }
 

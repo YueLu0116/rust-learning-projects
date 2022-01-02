28cdcf557db19dd1be7bee0e4e5a4af5143cde40
YueLu0116 <bit_drinkwater@163.com>
2022-01-03 01:35:48 +0800

    Finish ubuntu Kindle device detection using media path

--- SimpleKindleMate/src/utils.rs
+++ SimpleKindleMate/src/utils.rs
@@ -1,9 +1,10 @@
 use std::fs;
 use std::env;
+use whoami;
 
 pub mod constants{
     pub const PATH_MAC : &str = "/Volumes/Kindle/system/version.txt";
-    pub const PATH_LINUX : &str = ""; // TODO
+    pub const PATH_LINUX : &str = "/media";
     pub const PATH_WIN : &str = "";   // TODO
 }
 
@@ -11,26 +12,27 @@ pub mod constants{
 pub mod device{
     use super::*;
     pub fn device_info() -> Result<(), &'static str>{
-        let path : &str;
+        let path : String;
         match env::consts::OS{
             "macos" => {
-                path = constants::PATH_MAC;
+                path = constants::PATH_MAC.to_owned();
             }
             "linux" =>{
-                path = constants::PATH_LINUX;
+                let user_name = &whoami::username()[..];
+                path = format!("{}/{}/Kindle/system/version.txt", constants::PATH_LINUX, user_name);
             }
             "windows"=>{
-                path = constants::PATH_WIN;
+                path = constants::PATH_WIN.to_owned();
             }
             _ =>{
                 return Err("Unsupported platform!");
             }
         }
-        if let Ok(_file) = fs::File::open(path){
+        if let Ok(_file) = fs::File::open(&path){
             if let Ok(content) = fs::read_to_string(path) {
                 println!("device: {}", content);
             } else{
-                return Err("Cannot get device information. Read file error.");
+                return Err("Cannot get device information. Read file {} error.");
             }
         } else{
             return Err("No device detected. Is it connected?");

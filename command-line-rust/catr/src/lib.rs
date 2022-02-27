bf7e244707a1fbfd09d468e105aad7c20877836e
YueLu0116 <bit_drinkwater@163.com>
2022-02-27 19:10:59 +0800

    Add: Finish catr

--- command-line-rust/catr/src/lib.rs
+++ command-line-rust/catr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -11,25 +13,54 @@ pub struct Config{
 }
 
 pub fn run(config: Config) -> MyResult<()>{
-    dbg!(config);
+    for filename in config.files{
+        match open(&filename){
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num,line) in file.lines().enumerate(){
+                    let disp = line?;
+                    if config.number_lines{
+                        println!("{:>6}\t{}", line_num+1, &disp);
+                    }else if config.number_nonblank_lines {
+                        if !disp.is_empty(){
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, &disp);
+                        }else{
+                            println!();
+                        }
+                    }else{
+                        println!("{}", &disp);
+                    }                    
+                }
+            },
+        }
+    }
     Ok(())
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>>{
+    match filename{
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("catr")
         .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .author("Yue Lu <dereklu19960116@gmail.com>")
         .about("Rust cat")
         .arg(
-            Arg::with_name("files")
+            Arg::new("files")
                 .allow_invalid_utf8(true)
                 .value_name("FILE")
                 .help("Input file(s)")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .default_value("-"),
         )
         .arg(
-            Arg::with_name("number")
+            Arg::new("number")
                 .short('n')
                 .long("number")
                 .help("Number lines")
@@ -37,7 +68,7 @@ pub fn get_args() -> MyResult<Config> {
                 .conflicts_with("number_nonblank"),
         )
         .arg(
-            Arg::with_name("number_nonblank")
+            Arg::new("number_nonblank")
                 .short('b')
                 .long("number-nonblank")
                 .help("Number non-blank lines")

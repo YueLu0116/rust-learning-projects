5ca464120881e04a811b6e0ee9fe811d121224a5
YueLu0116 <bit_drinkwater@163.com>
2022-03-02 07:58:43 +0800

    Add: finish string2usize

--- command-line-rust/headr/src/lib.rs
+++ command-line-rust/headr/src/lib.rs
@@ -14,3 +14,24 @@ pub fn get_config() -> MyResult<Config>{
 
     unimplemented!()
 }
+
+fn string2usize(input : &str) ->MyResult<usize>{
+    match input.parse::<usize>(){
+        Ok(num) if num > 0 => Ok(num),  // usize > 0
+        _ => Err(input.into())
+    }
+}
+
+#[test]
+fn test_string2usize(){
+    let input = "3";
+    assert!(string2usize(input).is_ok());
+    assert_eq!(string2usize(input).unwrap(), 3);
+
+    let invalid_input = "foo";
+    assert!(string2usize(invalid_input).is_err());
+
+    let zero_input = "0";
+    assert!(string2usize(zero_input).is_err());
+}
+
